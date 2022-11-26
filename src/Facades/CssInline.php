b35d4b1f0aa1ef74d4a8463dd11fd1a9c6dffb9f
Bradie <44430471+bradietilley@users.noreply.github.com>
2022-11-26 14:01:14 +0800

    docs: add docs to cssinline facade

--- src/Facades/CssInline.php
+++ src/Facades/CssInline.php
@@ -11,16 +11,34 @@ use Symfony\Component\Mime\Email;
 /**
  * @mixin CssInliner
  *
+ * @method static CssInliner debug(string $message)
  * @method static CssInliner addCssPath(string|SplFileInfo $file)
  * @method static CssInliner addCssRaw(string $css)
  * @method static CssInliner clearCss()
- * @method static CssInliner extractCssFromHtmlContent()
- * @method static CssInliner dontExtractCssFromHtmlContent()
+ * @method static bool emailListenerEnabled()
+ * @method static CssInliner enableEmailListener()
+ * @method static CssInliner disableEmailListener()
+ * @method static bool cssFromHtmlContentEnabled()
+ * @method static CssInliner enableCssExtractionFromHtmlContent()
+ * @method static CssInliner disableCssExtractionFromHtmlContent()
+ * @method static bool cssRemovalFromHtmlContentEnabled()
+ * @method static CssInliner enableCssRemovalFromHtmlContent()
+ * @method static CssInliner disableCssRemovalFromHtmlContent()
  * @method static string readCssFileAsString(string $file)
- * @method static Email convertEmail(string Email $email)
+ * @method static CssInliner interceptCssFile(string $file, callable $callback)
+ * @method static CssInliner interceptCssFiles(callable $callback)
+ * @method static CssInliner clearInterceptors()
+ * @method static Email convertEmail(Email $email)
  * @method static string convert(string $html)
- * @method static string parseCssFromHtml(string $html)
+ * @method static string parseCssFromHtml(string &$html)
+ * @method static CssInliner beforeConvertingEmail(callable $callback)
+ * @method static CssInliner afterConvertingEmail(callable $callback)
+ * @method static CssInliner beforeConvertingHtml(callable $callback)
+ * @method static CssInliner afterConvertingHtml(callable $callback)
  * @method static CssInliner instance()
+ * @method static array cssFiles()
+ * @method static array cssRaw()
+ * @method static void halt()
  */
 class CssInline extends Facade
 {
